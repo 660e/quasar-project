4f591c35f96a52c8ee75fbbf9697326a6c1f16cc
Min <solunar@vip.qq.com>
2024-03-12 21:15:13 +0800

    Update

---
+++ src/pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-5xl">404</div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({ name: 'app-404' });
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,15 +3,15 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+    component: () => import('layouts/index.vue'),
+    children: []
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
+    component: () => import('pages/404.vue')
   }
 ];
 
