588d8de68663a8ed5e2f1828525c71ed37c25eea
Min <solunar@vip.qq.com>
2024-03-26 21:57:49 +0800

    Update

--- src/layouts/components/menu.vue
+++ src/layouts/components/menu.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
 import { useLayoutStore } from '@/stores/modules/layout';
-import menu from '@/assets/json/menu.json';
+import menus from '@/assets/json/menus.json';
 
 defineOptions({ name: 'app-menu' });
 
 const $layoutStore = useLayoutStore();
 
-console.log(menu);
+console.log(menus);
 </script>
 
 <template>
-  <q-drawer v-model="$layoutStore.drawer" :width="200" bordered>app-menu</q-drawer>
+  <q-drawer v-model="$layoutStore.drawer" :width="220" bordered>
+    <q-list>
+      <template v-for="menu in menus" :key="menu.name">
+        <q-expansion-item v-if="menu.children?.length" :label="menu.meta.label">
+          <q-list>
+            <q-item v-for="m in menu.children" :key="m.name" clickable v-ripple>
+              <q-item-section>{{ m.meta.label }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+        <q-item v-else clickable v-ripple>
+          <q-item-section>{{ menu.meta.label }}</q-item-section>
+        </q-item>
+      </template>
+    </q-list>
+  </q-drawer>
 </template>
