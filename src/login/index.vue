4770a6c2a585042a71182d25f116f6554d7a8a87
Min <solunar@vip.qq.com>
2024-03-28 22:07:55 +0800

    Update

--- src/login/index.vue
+++ src/login/index.vue
@@ -1,7 +1,34 @@
 <script lang="ts" setup>
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
 defineOptions({ name: 'app-login' });
+
+const form = reactive({
+  username: 'admin',
+  password: '123456'
+});
+
+const $router = useRouter();
+const loading = ref(false);
+const signIn = () => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+    $router.push({ name: 'home' });
+  }, 3000);
+};
 </script>
 
 <template>
-  <div>Lorem ipsum dolor sit amet.</div>
+  <q-card class="h-screen flex justify-center" flat>
+    <div class="flex flex-col items-center">
+      <img src="@/assets/quasar-logo-vertical.svg" class="w-40" />
+      <div class="w-80 border border-gray-300 bg-gray-100 rounded p-4 space-y-4">
+        <q-input v-model="form.username" :disable="loading" label="Username" class="bg-white" outlined />
+        <q-input v-model="form.password" :disable="loading" label="Password" class="bg-white" type="password" outlined />
+        <q-btn :loading="loading" @click="signIn" label="Sign in" color="primary" class="w-full" no-caps unelevated />
+      </div>
+    </div>
+  </q-card>
 </template>
